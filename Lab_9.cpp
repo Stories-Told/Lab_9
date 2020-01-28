9a3f7cfe245e0595a64ef49fc552c4edc9ac4d5d
Stories-Told <54217603+Stories-Told@users.noreply.github.com>
2020-01-28 15:57:21 -0500

    Add files via upload

---
+++ Lab_9.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct computerType
+    {
+        string manufacturer;
+        string modelType;
+        string processorType;
+        int ram;
+        int hddGB;
+        int year;
+        double price;
+    } computerType;
+
+int main()
+{
+    cout << "-------------------------------------\n";
+    cout << "Enter your computers INFO as PROMPTED\n";
+    cout << "-------------------------------------\n";
+
+    cout << "Manufacturer: ";
+    cin >> computerType.manufacturer;
+    cout << endl;
+
+    cout << "Model: ";
+    cin >> computerType.modelType;
+    cout << endl;
+
+    cout << "CPU: ";
+    cin >> computerType.processorType;
+    cout << endl;
+
+    cout << "RAM: ";
+    cin >> computerType.ram;
+    cout << endl;
+
+    cout << "HDD Size(Gb): ";
+    cin >> computerType.hddGB;
+    cout << endl;
+
+    cout << "Year: ";
+    cin >> computerType.year;
+    cout << endl;
+
+    cout << "Price: $ ";
+    cin >> computerType.price;
+    cout << endl;
+
+    cout << "Thank you, this information will be shared "
+         << "with retailers to better provide you with ads."
+         << "\n\n";
+
+    cout << "Information provided:\n"
+         << "Manufacturer: " << computerType.manufacturer << endl
+         << "Model: " << computerType.modelType << endl
+         << "CPU: " << computerType.processorType << endl
+         << "RAM: " << computerType.ram << endl
+         << "HDD(Gb): " << computerType.hddGB << endl
+         << "Year: " << computerType.year << endl
+         << "Price: $" << computerType.price << endl;
+
+
+    return 0;
+}
